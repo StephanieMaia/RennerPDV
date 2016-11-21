7c4b8159ebe145ee595850d582992bcc2e26e50c
Stephanie <marcidamaia@gmail.com>
2016-11-21 21:51:04 -0200

    Delete

--- js/produtos.js
+++ js/produtos.js
@@ -1,32 +1,12 @@
 $(document).ready(function() {  //lê todos os documentos ao final de td
 
-    $.getJSON('/model/produtos.php', function(retorno) {
-        $('#lista-produtos tbody').empty();
-        
-        var total = 0;
+    carregaRegistros();
 
-        retorno.forEach(function(obj, idx) {           
-            total += parseInt(obj.quantidade); //mudando string para inteiro
-
-            var tr = "<tr>"
-                    + "<td>" + obj.codigo + "</td>"
-                    + "<td>" + obj.nome + "</td>"
-                    + "<td>" + obj.marca + "</td>"
-                    + "<td>R$ " + formataValor(obj.preco) + "</td>"
-                    + "<td>" + obj.quantidade + "</td>";
-            "  </tr>";
-            $('#lista-produtos tbody').append(tr);
-        });
-        
-        $('#quantidade-total').html(total);
-        
-    });
 
     //código para permitir que sejam digitados apenas números.
-
     $('#codigo, #valor, #quantidade').keydown(function(key) {
         console.log(key.keyCode);
-        if (key.keyCode >= 48 && key.keyCode <= 57) {           
+        if (key.keyCode >= 48 && key.keyCode <= 57) {
             return true;
         } else if (key.keyCode === 8 || key.keyCode === 9) {
 
@@ -37,20 +17,27 @@ $(document).ready(function() {  //lê todos os documentos ao final de td
 
     });
     //código para permitir que sejam digitados apenas numeros.
-$('#bt-cadastrar').click(function(){
-    $('#form-produto').submit(); ///chama o submit primeiro e depois da o alerta para não replicar a mensagem para o usúario.
-});
-    $('#form-produto').submit(function(evento){ //mostra o alert para o usúario
+    $('#bt-cadastrar').click(function() {
+        $('#form-produto').submit(); ///chama o submit primeiro e depois da o alerta para não replicar a mensagem para o usúario.
+    });
+    $('#form-produto').submit(function(evento) { //mostra o alert para o usúario
         evento.preventDefault(); //para não redirecionar, passar por debaixo dos panos
         var dados = {
-            
+            codigo: $('#codigo').val(),
+            nome: $('#nome').val(),
+            marca: $('#marca').val(),
+            valor: $('#valor').val(),
+            quantidade: $('#quantidade').val()
+
         };
-        $.post("/model/cadastro.php", dados, function(retorno){ //função de callback para permanecer na mesma página
-           
+        $.post("/model/cadastro.php", dados, function(retorno) { //função de callback para permanecer na mesma página
+
             var obj_retorno = JSON.parse(retorno); //transformando mensagem de texto (status ok em cadastro.php) em objeto
-         
-            if (obj_retorno.status == "ok"){
-                alert("cadastrado com sucesso! ") //se status esta ok, mostra mensagem
+
+            if (obj_retorno.status === "ok") {
+                $('#cadastro-produto').modal('hide');
+                carregaRegistros();
+                $('#alert-produto').removeClass('hide');
             }
         });
     });
@@ -68,4 +55,38 @@ function formataValor(valor) {
     var retorno = partes[0] + ',' + partes[1];
 
     return retorno;
+}
+function carregaRegistros() {
+    $.getJSON('/model/produtos.php', function(retorno) {
+        $('#lista-produtos tbody').empty();
+
+        var total = 0;
+
+        retorno.forEach(function(obj, idx) {
+            total += parseInt(obj.quantidade); //mudando string para inteiro
+
+            var tr = '<tr obj-id="'+ obj.id +'">'
+                    + "<td>" + obj.codigo + "</td>"
+                    + "<td>" + obj.nome + "</td>"
+                    + "<td>" + obj.marca + "</td>"
+                    + "<td>R$ " + formataValor(obj.preco) + "</td>"
+                    + "<td>" + obj.quantidade + "</td>"
+                    + '<td><button type="button" class="bt-deletar btn btn-danger">'
+                    + '<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>'   //botão excluir(glif)
+                    + '</button>'
+                    + "</td>";
+            "  </tr>";
+            $('#lista-produtos tbody').append(tr);
+        });
+
+        $('#quantidade-total').html(total);
+
+        $('.bt-deletar').click(function() { 
+            var tr = $(this).parent().parent(); //pegando id
+            var id = tr.attr('obj-id');
+            
+        });
+
+    });
+
 }
---
+++ model/deletar.php
@@ -0,0 +1,5 @@
+<?php
+$id = $_GET["id"];
+$sql = "DELETE FROM produtos WHERE id = $id";
+
+echo $sql;
